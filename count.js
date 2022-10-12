60c4205b765e21bc59dff2f6a06b15bfc3b8c3cc
nennneko5787 <95109758+nennneko5787@users.noreply.github.com>
2022-10-12 18:45:45 +0900

    Delete count.js

--- count.js
+++
@@ -1,4 +0,0 @@
-var now = new Date();
-var xxday = new Date(2022,7-4,23);
-count2 = Math.ceil( (now.getTime() - xxday.getTime()) / (24*60*60*1000) );
-document.write("14‚¿‚á‚ñ‚Ë‚éŠJİ‚©‚ç" + count2 + "“ú–Ú");
